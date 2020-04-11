37fde3e67dcdcbc8cbb8cb0a1b0d1cb9558067c4
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-04-11 10:39:05 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+test_requirements = ["codecov", "pytest", "pytest-cov", "pytest-raises"]
+
+docs_requirements = ["sphinx==1.8.5", "sphinx-rtd-theme"]
+
+setup_requirements = ["pytest-runner"]
+
+dev_requirements = [
+    *test_requirements,
+    *docs_requirements,
+    *setup_requirements,
+    "pre-commit",
+    "bumpversion>=0.5.3",
+    "ipython>=7.5.0",
+    "tox>=3.5.2",
+    "twine>=1.13.0",
+    "wheel>=0.33.1",
+]
+
+interactive_requirements = ["altair", "jupyterlab", "matplotlib"]
+
+requirements = []
+
+extra_requirements = {
+    "test": test_requirements,
+    "docs": docs_requirements,
+    "setup": setup_requirements,
+    "dev": dev_requirements,
+    "interactive": interactive_requirements,
+    "all": [
+        *requirements,
+        *test_requirements,
+        *docs_requirements,
+        *setup_requirements,
+        *dev_requirements,
+        *interactive_requirements,
+    ],
+}
+
+setup(
+    author="Jacob Moorman",
+    author_email="jacob@moorman.me",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Fast constrained linear assignment problem (CLAP) solvers for Python",
+    entry_points={"console_scripts": ["my_example=clapsolver.bin.my_example:main"]},
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="clapsolver",
+    name="clapsolver",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    extras_require=extra_requirements,
+    url="https://github.com/jdmoorman/clapsolver",
+    # Do not edit this string manually, always use bumpversion
+    # Details in CONTRIBUTING.rst
+    version="0.1.0",
+    zip_safe=False,
+)
---
+++ src/clapsolver/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for clapsolver."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.1.0"
+
+__author__ = "Jacob Moorman"
+__email__ = "jacob@moorman.me"
+
+__license__ = "MIT license".rstrip(" license")
+__copyright__ = "Copyright (c) 2020, Jacob Moorman"
+
+
+from .example import Example  # noqa: F401
