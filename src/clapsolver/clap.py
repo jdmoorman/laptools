bfd6531da497175a3b565734937b93cde19e08ae
Qinyi Chen <qinyichen@g.ucla.edu>
2020-04-30 21:38:10 -0700

    Renamed things

--- src/clapsolver/clap.py
+++ src/clapsolver/clap.py
@@ -4,7 +4,7 @@ from scipy.optimize import linear_sum_assignment as lap
 from ._util import one_hot
 
 
-def cost(i, j, costs):
+def cost(i, j, cost_matrix):
     """Compute the minimal cost over all constrained assignments.
 
     Parameters
@@ -13,7 +13,7 @@ def cost(i, j, costs):
         Row index corresponding to the constraint.
     j : int
         Column index corresponding to the constraint.
-    costs : 2darray
+    cost_matrix : 2darray
         A matrix of costs.
 
     Returns
@@ -22,20 +22,20 @@ def cost(i, j, costs):
         The total cost of the linear assignment problem solution under the
         constraint that row i is assigned to column j.
     """
-    costs = np.array(costs)
-    n_rows, n_cols = costs.shape
+    cost_matrix = np.array(cost_matrix)
+    n_rows, n_cols = cost_matrix.shape
 
     # Cost matrix omitting the row and column corresponding to the constraint.
-    sub_costs = costs[~one_hot(i, n_rows), :][:, ~one_hot(j, n_cols)]
+    sub_cost_matrix = cost_matrix[~one_hot(i, n_rows), :][:, ~one_hot(j, n_cols)]
 
     # Lsap solution for the submatrix.
-    sub_row_ind, sub_col_ind = lap(sub_costs)
+    sub_row_ind, sub_col_ind = lap(sub_cost_matrix)
 
     # Total cost is that of the submatrix lsap plus the cost of the constraint.
-    return sub_costs[sub_row_ind, sub_col_ind].sum() + costs[i, j]
+    return sub_cost_matrix[sub_row_ind, sub_col_ind].sum() + cost_matrix[i, j]
 
 
-def constrained_lsap_costs(costs):
+def costs(cost_matrix):
     """Solve a constrained linear sum assignment problem for each entry.
 
     TODO: More thorough testing of this function.
@@ -43,15 +43,15 @@ def constrained_lsap_costs(costs):
     The output of this function is equivalent to, but significantly more
     efficient than,
 
-    >>> def constrained_lsap_costs(costs):
-    ...     total_costs = np.empty_like(costs)
-    ...     for i, j in np.ndindex(*costs.shape):
-    ...         total_costs[i, j] = constrained_lsap_cost(i, j, costs)
+    >>> def costs(cost_matrix):
+    ...     total_costs = np.empty_like(cost_matrix)
+    ...     for i, j in np.ndindex(*cost_matrix.shape):
+    ...         total_costs[i, j] = cost(i, j, cost_matrix)
     ...     return total_costs
 
     Parameters
     ----------
-    costs : 2darray
+    cost_matrix : 2darray
         A matrix of costs.
 
     Returns
@@ -61,21 +61,21 @@ def constrained_lsap_costs(costs):
         corresponds to the total lsap cost under the constraint that row i is
         assigned to column j.
     """
-    n_rows, n_cols = costs.shape
+    n_rows, n_cols = cost_matrix.shape
     if n_rows > n_cols:
-        return constrained_lsap_costs(costs.T).T
+        return costs(cost_matrix.T).T
 
     # Find the best lsap assignment from rows to columns without constrains.
     # Since there are at least as many columns as rows, row_idxs should
     # be identical to np.arange(n_rows). We depend on this.
-    row_idxs, lsap_col_idxs = lap(costs)
+    row_idxs, lsap_col_idxs = lap(cost_matrix)
 
     # Column vector of costs of each assignment in the lsap solution.
-    lsap_costs = costs[row_idxs, lsap_col_idxs]
+    lsap_costs = cost_matrix[row_idxs, lsap_col_idxs]
     lsap_total_cost = lsap_costs.sum()
 
     # Each row is column indexes ordered by their costs in that row.
-    sorted_costs_ind = np.argsort(costs, axis=1)
+    sorted_costs_ind = np.argsort(cost_matrix, axis=1)
 
     # When a row has its column stolen by a constraint, these are the columns
     # that might come into play when we are forced to resolve the assignment.
@@ -89,7 +89,7 @@ def constrained_lsap_costs(costs):
     # If additionally column j is not in lsap_col_idxs, it is not taken away
     # from any of the other row assignments. In this situation, the resulting
     # total assignment costs are:
-    total_costs = lsap_total_cost - lsap_costs[:, None] + costs
+    total_costs = lsap_total_cost - lsap_costs[:, None] + cost_matrix
 
     for i, freed_j in enumerate(lsap_col_idxs):
         # For each row, which column is it currently assigned to? Modify this
@@ -100,26 +100,26 @@ def constrained_lsap_costs(costs):
 
         # When row i is constrained to another column, can column j be
         # reassigned to improve the assignment cost of one of the other rows?
-        freed_col_costs = costs[:, freed_j]
+        freed_col_costs = cost_matrix[:, freed_j]
         if np.any(freed_col_costs < lsap_costs):
             # Solve the lsap with row i omitted. For the majority of
             # constraints on row i's assignment, this will not conflict with
             # the constraint. When it does conflict, we fix the issue later.
             sub_ind = ~one_hot(i, n_rows)
-            sub_costs = costs[sub_ind, :][:, lsap_col_idxs]
-            sub_row_ind, sub_col_ind = lap(sub_costs)
-            sub_total_cost = sub_costs[sub_row_ind, sub_col_ind].sum()
+            sub_cost_matrix = cost_matrix[sub_ind, :][:, lsap_col_idxs]
+            sub_row_ind, sub_col_ind = lap(sub_cost_matrix)
+            sub_total_cost = sub_cost_matrix[sub_row_ind, sub_col_ind].sum()
             col_idxs[sub_ind] = lsap_col_idxs[sub_col_ind]
 
             # This calculation will end up being wrong for the columns in
             # lsap_col_idxs[sub_col_ind]. This is because the constraint in
             # row i in these columns will conflict with the sub assignment.
             # These miscalculations are corrected later.
-            total_costs[i, :] = costs[i, :] + sub_total_cost
+            total_costs[i, :] = cost_matrix[i, :] + sub_total_cost
 
         # col_idxs now contains the optimal assignment columns ignoring row i.
         col_idxs[i] = np.setdiff1d(lsap_col_idxs, col_idxs)[0]
-        total_costs[i, col_idxs[i]] = costs[row_idxs, col_idxs].sum()
+        total_costs[i, col_idxs[i]] = cost_matrix[row_idxs, col_idxs].sum()
         col_idxs[i] = -1
 
         for other_i, stolen_j in enumerate(col_idxs):
@@ -139,11 +139,11 @@ def constrained_lsap_costs(costs):
             # solve the constrained lsap from scratch.
             if next_best_j not in col_idxs:
                 col_idxs[other_i] = next_best_j
-                total_costs[i, stolen_j] = costs[row_idxs, col_idxs].sum()
+                total_costs[i, stolen_j] = cost_matrix[row_idxs, col_idxs].sum()
             else:
-                sub_costs = costs[:, potential_cols]
+                sub_cost_matrix = cost_matrix[:, potential_cols]
                 sub_j = np.argwhere(potential_cols == stolen_j)[0]
-                total_cost = constrained_lsap_costs(i, sub_j, sub_costs)
+                total_cost = costs(i, sub_j, sub_cost_matrix)
                 total_costs[i, stolen_j] = total_cost
 
             # Give other_i its column back in preparation for the next round.
--- tests/test_clap_cost.py
+++ tests/test_clap_cost.py
@@ -3,41 +3,57 @@ import pytest
 from clapsolver import clap
 
 # fmt: off
-costs1 = [[4, 1, 3],
-          [2, 0, 5],
-          [3, 2, 2]]
+cost_matrix_1 = [[4, 1, 3],
+                 [2, 0, 5],
+                 [3, 2, 2]]
 
-costs2 = [[4, 1, 3, 6],
-          [2, 0, 5, 7],
-          [3, 2, 2, 8]]
+cost_matrix_2 = [[4, 1, 3, 6],
+                 [2, 0, 5, 7],
+                 [3, 2, 2, 8]]
 
-costs3 = [[4, 1, 3],
-          [2, 0, 5],
-          [3, 2, 2],
-          [6, 7, 8]]
+cost_matrix_3 = [[4, 1, 3],
+                 [2, 0, 5],
+                 [3, 2, 2],
+                 [6, 7, 8]]
 # fmt: on
 
 
 @pytest.mark.parametrize(
-    "i, j, costs, expected",
+    "i, j, cost_matrix, expected",
     [
-        (0, 0, costs1, 6),
-        (0, 1, costs1, 5),
-        (0, 2, costs1, 6),
-        (1, 0, costs1, 5),
-        (1, 1, costs1, 6),
-        (1, 2, costs1, 9),
-        (2, 0, costs1, 6),
-        (2, 1, costs1, 7),
-        (2, 2, costs1, 5),
-        (0, 3, costs2, 8),
-        (1, 3, costs2, 10),
-        (2, 3, costs2, 11),
-        (3, 0, costs3, 8),
-        (3, 1, costs3, 11),
-        (3, 2, costs3, 11),
+        (0, 0, cost_matrix_1, 6),
+        (0, 1, cost_matrix_1, 5),
+        (0, 2, cost_matrix_1, 6),
+        (1, 0, cost_matrix_1, 5),
+        (1, 1, cost_matrix_1, 6),
+        (1, 2, cost_matrix_1, 9),
+        (2, 0, cost_matrix_1, 6),
+        (2, 1, cost_matrix_1, 7),
+        (2, 2, cost_matrix_1, 5),
+        (0, 3, cost_matrix_2, 8),
+        (1, 3, cost_matrix_2, 10),
+        (2, 3, cost_matrix_2, 11),
+        (3, 0, cost_matrix_3, 8),
+        (3, 1, cost_matrix_3, 11),
+        (3, 2, cost_matrix_3, 11),
     ],
 )
-def test_clap_cost(i, j, costs, expected):
+def test_clap_cost(i, j, cost_matrix, expected):
     """Verify clap.cost works on small examples computable by hand."""
-    assert clap.cost(i, j, costs) == expected
+    assert clap.cost(i, j, cost_matrix) == expected
+
+
+#
+# @pytest.mark.parametrize(
+#     "costs, lsap_costs",
+#     [
+#         (costs1, 6),
+#         (costs1, 5),
+#         (costs1, 6),
+#         (costs1, 5),
+#     ],
+# )
+#
+# def test_clap_constrained_lsap_costs(costs, lsap_costs):
+#     """Verify clap.constrained_lsap_costs works on small examples."""
+#     assert np.all(clap.constrained_lsap_cost(costs) == lsap_costs)
