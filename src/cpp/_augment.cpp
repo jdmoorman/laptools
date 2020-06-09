794bb613f5ea2e8237f8aab48e0b357a63d8a759
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-06-09 12:14:42 -0700

    Add a function that uses numpy array in c++. Only works with doubles.

--- src/cpp/_augment.cpp
+++ src/cpp/_augment.cpp
@@ -1,10 +1,18 @@
 #include <pybind11/pybind11.h>
-
-int augment(int i, int j) {
-    return i + j;
-}
+#include <pybind11/numpy.h>
 
 namespace py = pybind11;
+
+double augment(py::array_t<double> cost_matrix) {
+    auto cost_data = cost_matrix.unchecked<2>();
+    double sum = 0;
+    for (size_t i = 0; i < cost_data.shape(0); i++) {
+        for (size_t j = 0; j < cost_data.shape(1); j++) {
+            sum += cost_data(i, j);
+        }
+    }
+    return sum;
+}
 
 PYBIND11_MODULE(_augment, m) {
     m.def("augment", &augment, R"pbdoc(
