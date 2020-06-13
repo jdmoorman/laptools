3ff1756c6e027930340bc422fdb63cbfa95def94
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-06-13 10:52:56 -0700

    Fix license string. Trigger GH actions.

--- src/laptools/__init__.py
+++ src/laptools/__init__.py
@@ -7,7 +7,7 @@ __version__ = "0.1.8"
 __author__ = "Jacob Moorman"
 __email__ = "jacob@moorman.me"
 
-__license__ = "MIT license".rstrip(" license")
+__license__ = "MIT"
 __copyright__ = "Copyright (c) 2020, Jacob Moorman"
 
 
