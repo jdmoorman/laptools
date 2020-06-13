e9c84e63e0e5c100632e5cb7ce9ddf832dd154f5
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-06-13 11:41:50 -0700

    Check dtypes and ids in test_augment. Debug windows failure.

--- tests/test_cpp.py
+++ tests/test_cpp.py
@@ -4,7 +4,7 @@ from _augment import augment
 
 
 def test_augment():
-    """Dummy test that checks whether augment modifies input values."""
+    """Augment should modify its arguments inplace."""
     cost_matrix = np.array([[0.0, 0.0, 0.0], [0.0, 2.0, 5.0], [2.0, 3.0, 2.0]])
     row4col = np.array([-1, 1, 2])
     col4row = np.array([-1, 1, 2])
@@ -12,8 +12,11 @@ def test_augment():
     v = np.array([-2.0, 0.0, 0.0])
 
     row4col_copy, col4row_copy = row4col.copy(), col4row.copy()
+    inputs = [row4col, col4row, u, v]
+    outputs = augment(cost_matrix, 0, row4col, col4row, u, v)
 
-    augment(cost_matrix, 0, row4col, col4row, u, v)
+    assert [elm.dtype for elm in inputs] == [elm.dtype for elm in outputs]
+    assert [id(elm) for elm in inputs] == [id(elm) for elm in outputs]
 
     assert row4col_copy.tolist() != row4col.tolist()
     assert col4row_copy.tolist() != col4row.tolist()
