1af1bc91d0bc1dfcaf066075d87e5c73cdc7b392
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-07-01 15:18:08 -0700

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -202,6 +202,6 @@ setup(
     url="https://github.com/jdmoorman/laptools",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.2.1",
+    version="0.2.2",
     zip_safe=False,
 )
