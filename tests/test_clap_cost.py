bbb39c764378b405cfb4c949527f7640fa2b67c8
Qinyi Chen <qinyichen@g.ucla.edu>
2020-07-08 11:52:14 -0700

    Update tests

--- tests/test_clap_cost.py
+++ tests/test_clap_cost.py
@@ -7,17 +7,19 @@ cost_matrices = []
 global_cost_matrices = []
 
 # fmt: off
+
+# cost_matrix0-expected_global_costs0
 cost_matrices.append(
     [[4, 1, 3],
      [2, 0, 5],
      [3, 2, 2]])
-
 
 global_cost_matrices.append(
     [[6, 5, 6],
      [5, 6, 9],
      [6, 7, 5]])
 
+# cost_matrix1-expected_global_costs1
 cost_matrices.append(
     [[4, 1, 3, 6],
      [2, 0, 5, 7],
@@ -28,7 +30,7 @@ global_cost_matrices.append(
      [5, 6, 9, 10],
      [6, 7, 5, 11]])
 
-
+# cost_matrix2-expected_global_costs2
 cost_matrices.append(
     [[4, 1, 3],
      [2, 0, 5],
@@ -41,7 +43,7 @@ global_cost_matrices.append(
      [6, 7, 5],
      [8, 11, 11]])
 
-
+# cost_matrix3-expected_global_costs3
 cost_matrices.append(
     [[np.inf, np.inf, np.inf, np.inf,      3],
      [     7, np.inf,     23, np.inf, np.inf],
@@ -54,7 +56,7 @@ global_cost_matrices.append(
      [    49,     47, np.inf, np.inf, np.inf],
      [np.inf,     49,     47,     54, np.inf]])
 
-
+# cost_matrix4-expected_global_costs4
 cost_matrices.append(
     [[4, 1, 3, np.inf],
      [2, 0, 5, np.inf],
@@ -65,7 +67,7 @@ global_cost_matrices.append(
      [5, 6, 9, np.inf],
      [6, 7, 5, np.inf]])
 
-
+# cost_matrix5-expected_global_costs5
 cost_matrices.append(
     [[4     , 1     ,      3],
      [2     , 0     ,      5],
@@ -80,7 +82,7 @@ global_cost_matrices.append(
      [6, 7, 5],
      [8, 11, 11]])
 
-
+# cost_matrix6-expected_global_costs6
 cost_matrices.append(
     [[0     , 0,      0],
      [np.inf, 0, np.inf],
@@ -91,8 +93,7 @@ global_cost_matrices.append(
      [np.inf,      0, np.inf],
      [     0, np.inf,      0]])
 
-# New tests:
-
+# cost_matrix7-expected_global_costs7
 cost_matrices.append(
     [[3, 8, 1, 3, 8],
      [2, 0, 6, 8, 9],
@@ -107,7 +108,7 @@ global_cost_matrices.append(
      [17, 16, 18, 10, 10],
      [10, 19, 15, 13, 12]])
 
-
+# cost_matrix8-expected_global_costs8
 cost_matrices.append(
     [[4,      1,      3],
      [2, np.inf, np.inf],
